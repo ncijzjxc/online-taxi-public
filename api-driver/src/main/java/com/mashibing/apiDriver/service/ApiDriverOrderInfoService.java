787ae40d632817464930d4479fc519dfbcb4f486
mz <1774114978@qq.com>
2026-03-01 10:49:22 +0800

    26.3.1提交

---
+++ api-driver/src/main/java/com/mashibing/apiDriver/service/ApiDriverOrderInfoService.java
@@ -0,0 +1,55 @@
+package com.mashibing.apiDriver.service;
+
+import com.mashibing.apiDriver.remote.ServiceOrderClient;
+import com.mashibing.internalcommon.constant.IdentityConstants;
+import com.mashibing.internalcommon.constant.OrderConstants;
+import com.mashibing.internalcommon.dto.OrderInfo;
+import com.mashibing.internalcommon.dto.ResponseResult;
+import com.mashibing.internalcommon.request.OrderRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.time.LocalDateTime;
+
+
+@Service
+public class ApiDriverOrderInfoService {
+
+    @Autowired
+    ServiceOrderClient serviceOrderClient;
+
+    public ResponseResult toPickUpPassenger(OrderRequest orderRequest){
+        return serviceOrderClient.toPickUpPassenger(orderRequest);
+    }
+
+    public ResponseResult arrivedDeparture(OrderRequest orderRequest){
+        return serviceOrderClient.arrivedDeparture(orderRequest);
+    }
+
+    /**
+     * 司机接到乘客
+     * @param orderRequest
+     * @return
+     */
+    public ResponseResult pickUpPassenger(@RequestBody OrderRequest orderRequest){
+        return  serviceOrderClient.pickUpPassenger(orderRequest);
+    }
+
+    public ResponseResult passengerGetoff(OrderRequest orderRequest){
+        return serviceOrderClient.passengerGetoff(orderRequest);
+    }
+
+    public ResponseResult cancel(Long orderId){
+        return  serviceOrderClient.cancel(orderId, IdentityConstants.DRIVER_IDENTITY);
+    }
+
+    public ResponseResult<OrderInfo> detail(Long orderId){
+        return serviceOrderClient.detail(orderId);
+    }
+
+
+    public ResponseResult<OrderInfo> currentOrder(String phone , String identity){
+        return serviceOrderClient.current(phone,identity);
+    }
+}
---
+++ api-passenger/src/main/java/com/mashibing/apipassenger/controller/OrderController.java
@@ -0,0 +1,60 @@
+package com.mashibing.apipassenger.controller;
+
+import com.mashibing.apipassenger.remote.ServiceOrderClient;
+import com.mashibing.apipassenger.service.OrderService;
+import com.mashibing.internalcommon.constant.CommonStatusEnum;
+import com.mashibing.internalcommon.constant.IdentityConstants;
+import com.mashibing.internalcommon.dto.OrderInfo;
+import com.mashibing.internalcommon.dto.ResponseResult;
+import com.mashibing.internalcommon.dto.TokenResult;
+import com.mashibing.internalcommon.request.OrderRequest;
+import com.mashibing.internalcommon.util.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    @Autowired
+    OrderService orderService;
+    /**
+     * 创建订单/下单
+     * @return
+     */
+    @PostMapping("/add")
+    public ResponseResult add(@RequestBody OrderRequest orderRequest){
+        System.out.println(orderRequest);
+        return orderService.add(orderRequest);
+    }
+
+    /**
+     * 乘客取消订单
+     * @param orderId
+     * @return
+     */
+    @PostMapping("/cancel")
+    public ResponseResult cancel(@RequestParam Long orderId){
+        return orderService.cancel(orderId);
+    }
+
+    @GetMapping("/detail")
+    public ResponseResult<OrderInfo> detail(Long orderId){
+        return orderService.detail(orderId);
+    }
+
+    @GetMapping("/current")
+    public ResponseResult<OrderInfo> currentOrder(HttpServletRequest httpServletRequest){
+        String authorization = httpServletRequest.getHeader("Authorization");
+        TokenResult tokenResult = JwtUtils.parseToken(authorization);
+        String identity = tokenResult.getIdentity();
+        if (!identity.equals(IdentityConstants.PASSENGER_IDENTITY)){
+            return ResponseResult.fail(CommonStatusEnum.TOKEN_ERROR.getCode(),CommonStatusEnum.TOKEN_ERROR.getValue());
+        }
+        String phone = tokenResult.getPhone();
+
+        return orderService.currentOrder(phone,IdentityConstants.PASSENGER_IDENTITY);
+    }
+}
